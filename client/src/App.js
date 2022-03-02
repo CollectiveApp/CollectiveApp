37cbf41e0358fb90752f18bfe043ddd68b35b3b8
Lorelai <nadja.rathenow@rub.de>
2022-03-02 17:26:48 +0100

    routesVisitor

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pagesVisitor/Home'
+import Projects from './pagesVisitor/Projects'
+import ProjectDetails from './pagesVisitor/ProjectDetails'
+import Events from './pagesVisitor/Events'
+import Volunteer from './pagesVisitor/Volunteer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+  {/* Routes */}
+  <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/projects' element={<Projects />}/>
+    <Route path='/projects/:id' element={<ProjectDetails />}/>
+    <Route path='/events' element={<Events />}/>
+    <Route path='/volunteer' element={<Volunteer />}/>
+  </Routes>
     </div>
   );
 }
