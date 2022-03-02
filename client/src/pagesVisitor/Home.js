d47eb1d49b727b7e9713979102fd85fb81e15de6
Lorelai <nadja.rathenow@rub.de>
2022-03-02 18:22:41 +0100

    home and started on projects page from visitors side

--- client/src/pagesVisitor/Home.js
+++ client/src/pagesVisitor/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <img class="logo" src="" alt="Logo"/>
+      <p>Description</p>
+      <Link to={'/projects'}>Projects</Link>
+      <Link to={'/events'}>Events</Link>
+      <Link to={'/volunteer'}>Volunteer</Link>
     </div>
   )
 }
--- client/src/pagesVisitor/Projects.js
+++ client/src/pagesVisitor/Projects.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Projects() {
+
+  // State of Projects
+  const [projects, setProjects] = useState([])
+  console.log(projects)
+
+  // get all Projects from backend/server
+  const getAllProjects = () => {
+    //request to the local created api
+    axios.get('http://localhost:5005/api/projects')
+    .then(response => {
+      console.log(response.data)
+      //set the state of projects
+      setProjects(response.data)
+    })
+  }
+  useEffect(() => {
+    getAllProjects()
+  }, [])
+
   return (
     <div>
       <h1>Projects</h1>
+      {projects.map(project =>
+        <h1>{project.name}</h1>
+      )}
     </div>
   )
 }
