898a4e0c26d44ceebd57b913a6103705857d8a8f
Lorelai <nadja.rathenow@rub.de>
2022-03-03 12:19:45 +0100

    project Create change to compknents

---
+++ client/src/components/CreateEvent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function CreateEvent() {
+  return (
+    <>
+    <div>CreateProject</div>
+    <form>
+        <label>Name:</label>
+        <input type="text"></input>
+        
+    </form>
+    </>
+  )
+}
--- client/src/pagesAdmin/AdminDashboard.js
+++ client/src/pagesAdmin/AdminDashboard.js
@@ -1,29 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import CreateProject from '../components/CreateProject'
+import CreateEvent from '../components/CreateEvent'
 
 export default function AdminDashboard() {
+
+const [projects, setProjects] = useState([])
+const [events, setEvents] = useState([])
+const [showCreateProject, setShowCreateProject] = useState(false)
+const [showCreateEvent, setShowCreateEvent] = useState(false)
+
+
+const storedToken = localStorage.getItem('authToken')
+
+// Handler
+// const deleteProject = () => {
+//     axios.delete(`/api/projects/${_id}`)
+//         .then(() => {
+//             // redirect to the project list
+//             navigate('/projects')
+//         })
+//         .catch(err => console.log(err))
+// }
+
+// get all the projects from the backend / server
+const getAllProjects = () => {
+    // request 'api/projects'
+    // for every request to a project route we need to also send the token
+    axios.get('/api/projects', { headers: { Authorization: `Bearer ${storedToken}` } })
+        .then(response => {
+            console.log(response.data)
+            // set the state of projects
+            setProjects(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+useEffect(() => {
+    getAllProjects()
+}, [])
+
+const getAllEvents = () => {
+    // request 'api/events'
+    // for every request to a project route we need to also send the token
+    axios.get('/api/events', { headers: { Authorization: `Bearer ${storedToken}` } })
+        .then(response => {
+            console.log(response.data)
+            // set the state of projects
+            setEvents(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+useEffect(() => {
+    getAllEvents()
+}, [])
+
   return (
     <>
         <h1>Welcome to your Dashboard!</h1>
         <div>
-            <div>
-                <h1>Projects</h1>
-                <Link to={'/behind-the-scences/projects/create'}>Create new Project</Link>
-                {/* map over projects and return divs */}
-                <div>
-                    <Link to={'/behind-the-scences/project/edit/:id'}>Edit</Link>
-                    <Link to={'/behind-the-scences/project/delete/:id'}>Delete</Link>
-                </div>
-            </div>
-            <div>
-                <h1>Events</h1>
-                <Link to={'/behind-the-scences/event/create'}>Create new Event</Link>
-                {/* map over events and return divs */}
-                <div>
-                    <Link to={'/behind-the-scences/event/edit/:id'}>Edit</Link>
-                    <Link to={'/behind-the-scences/event/delete/:id'}>Delete</Link>
-                </div>
-            </div>
+            <>
+                <h1>All the projects</h1>
+                {projects.map(project => 
+                                        <>
+                                            <Link to={'/behind-the-scences/project/edit/:id'}>Edit</Link>
+                                            {/* <button onClick={deleteProject}>Delete this project</button> */}
+                                        </>
+                )}
+                <button onClick={() => setShowCreateProject(!showCreateProject)}>Create Project</button>
+                {showCreateProject && (
+                    <CreateProject refreshProjects={getAllProjects} />
+                )}
+            </>
+
+            <>
+                <h1>All the Events</h1>
+                {events.map(event => 
+                                        <>
+                                            <Link to={'/behind-the-scences/event/edit/:id'}>Edit</Link>
+                                            {/* <button onClick={deleteProject}>Delete this project</button> */}
+                                        </>
+                )}
+                <button onClick={() => setShowCreateEvent(!showCreateEvent)}>Create Event</button>
+                {showCreateEvent && (
+                    <CreateEvent refreshEvents={getAllEvents} />
+                )}
+            </>
         </div>
     </>
   )
