a0e924feee00cf0c350d88152fa45b7660461bd3
Lorelai <nadja.rathenow@rub.de>
2022-03-06 14:31:40 +0100

    required volunteer model in index.js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Admin = require('../models/Admin')
 const Event = require('../models/Event')
-const Helper = require('../models/Helper')
+const Volunteer = require('../models/Volunteer')
 const Project = require('../models/Project')
 
 
