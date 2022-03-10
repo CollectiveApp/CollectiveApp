e2f804a3a9cc256e3954f5882abfbc406ae5cdda
Lorelai <nadja.rathenow@rub.de>
2022-03-10 11:32:31 +0100

    app.js changes

--- app.js
+++ app.js
@@ -35,7 +35,13 @@ const volunteerRoutes = require('./routes/volunteers');
 app.use('/api/volunteers', volunteerRoutes, isAuthenticated)
 
 //this handles protected routes
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
 
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
